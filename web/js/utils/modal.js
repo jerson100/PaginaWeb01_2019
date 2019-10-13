8d6fea9c5d7a524ed12444814705730d25d37e7f
Jerson Ramírez Ortiz <juamkoo@gmail.com>
2019-10-13 03:37:40 -0500

    Haciendo algunos arreglos para mejorar la vista al momento de ver los likes de una publicación en concreta, para ello agregué estilos al archivo perfil.css, modifiqué un poco el modal.js ya que contenía mucho código que no sirve, se agregó también algunas imagenes de la aplicación misma, se agregó estilos para el scroll del body en el archivo base.css

--- web/js/utils/modal.js
+++ web/js/utils/modal.js
@@ -126,42 +126,65 @@ const openModalImg = (p) => {
         padre.addEventListener('click', e => {
             let elementP = e.target;//elemento pulsado
             if (elementP.tagName === 'IMG') {
-                let img = createCustomElement("img",{
-                    "class": "modal-img",
-                    "src" : elementP.src,
-                    "alt" : elementP.alt
-                });
-                let userImagenPost = createCustomElement("img",{
-                    "src":e.target.parentElement.parentElement.querySelector(".card_autor_img").src,
-                    "class": "img-user-post"
-                },[]);
-                let containerImage = createCustomElement("div", {"class": "containerImg"}, [img,userImagenPost]);
-                let titulo = createCustomElement("h3", {}, ['Usuarios que dieron like']);
-                let listaUsers = createCustomElement("ul", {"class": "list-users"});
-                let containerUsers = createCustomElement("div", {"class": "container-users"}, [listaUsers]);
-                let containerLike = createCustomElement("div", 
-                {
-                    "class": "containerLike",
-                    "style": "background-color:white;"
-                } , [titulo, containerUsers]);
 
-                let container = createCustomElement("div",
-                {
-                    "class":"modal-imagen",
-                    "style":"display = flex;"
-                },[containerImage,containerLike]);
-                
+                let footer = elementP.parentElement.nextElementSibling;
+
+                let container_img = createCustomElement("div", {"class": "modal-imagen", "style": "display_flex"}, []);
+
+                container_img.innerHTML = `
+                    <div class="containerImg">
+                        <img class="modal-img" src=${elementP.src} alt=${elementP.alt}>
+                        <!--<div class="container-details">
+                            <img src=${e.target.parentElement.parentElement.querySelector(".card_autor_img").src} class="img-user-post">
+                            <div class="post-details-username">
+                                <span>${elementP.previousElementSibling.firstElementChild.textContent}</span>
+                            </div>
+                            <div class="post-details-date">
+                            </div>
+                        </div>-->
+                    </div>
+                    <div class="containerLike" style="background-color:white;">
+                        <div class="post-details-container">
+                            <div class="post-details-inner">
+                                <div class="post-user_img">
+                                    <img src=${e.target.parentElement.parentElement.querySelector(".card_autor_img").src}></img>
+                                </div>
+                                <div class="post_info">
+                                    <a href="#"><span class="post_info_username">${footer.querySelector(".card_autor_name").textContent}</span></a>
+                                    <div class="post_info_hours">${footer.querySelector(".card_footer_fecha").firstElementChild.textContent}</div>
+                                </div>
+                            </div>
+                            <div class="container-countlikes">
+                                <span clas="count-likes">
+                                    <svg viewBox="0 0 17 15.736" class="svg-like" xmlns="http://www.w3.org/2000/svg"> 
+                                            <path d="M0,40.045H2.909V31.318H0Zm16-8a1.459,1.459,0,0,0-1.455-1.455H9.964l.727-3.345v-.218a1.525,1.525,0,0,0-.291-.8L9.6,25.5,4.8,30.3a1.22,1.22,0,0,0-.436,1.018v7.273a1.459,1.459,0,0,0,1.455,1.455h6.545a1.437,1.437,0,0,0,1.309-.873l2.182-5.164a1.238,1.238,0,0,0,.073-.509V32.045H16Z" transform="translate(0.5 -24.81)"></path>
+                                    </svg>
+                                    ${elementP.nextElementSibling.firstElementChild.textContent}
+                                </span>
+                            </div>
+                            <div class="container-users">
+                                <ul class="list-users">
+                                    <div class='loader-container loader-inter' id="loaderLike"></div>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                `;
+
+                let lista_users = container_img.querySelector(".list-users");
+
                 //Instanciamos un objeto de tipo modal, y le pasamos el container
                 //el cuál contendrá toda nuesto contenido de las imagenes y los usuarios
                 //que dieron like a esa determinada publicación
-                let modalm = new Modal(null, container, null, true, false);
+                let modalm = new Modal(null, container_img, null, true, true);
                 modalm.open();
                 modalm.container.firstElementChild.style.maxWidth = "100%";
                 modalm.container.firstElementChild.style.padding = "0";
+                modalm.container.firstElementChild.style.margin = "1rem 2rem";
                 document.body.classList.add("scrollOff");
 
-                //realizamos la petición ajax para ver los usuarios que dieron clicka  esa publicación
-                let title = e.target.previousElementSibling.firstElementChild.textContent;
+                let loader = document.getElementById("loaderLike");
+                loader.classList.add("loader-active");
                 let dat = new FormData();
                 dat.append("accion", "listarlikes");
                 dat.append("id", e.target.nextElementSibling.lastElementChild.textContent);
@@ -173,32 +196,57 @@ const openModalImg = (p) => {
                 };
                 AJAX.ajax(request).then((response) => {
                     let json = JSON.parse(response.responseText);
-                    console.log(json);
-                    printUsersLikes(json.usuarios, listaUsers);
-                }).catch((reject) => {
-                    console.log(reject);
+                    return printUsersLikes(json.usuarios, lista_users);
+                }).then(() => {//luego de realizar la petición y pintar quitamos el loader
+                    loader.classList.remove("loader-active");
+                }).catch((error) => {
+                    loader.classList.remove("loader-active");
+                    console.log("error: " + error);
                 });
-
             }
         });
     }
 
     const printUsersLikes = (users, listContainer) => {
-        let fragment = document.createDocumentFragment();
-        console.log(users);
-        users.forEach(e => {
-            let li = document.createElement("li");
-            li.setAttribute("class", "user-item");
-            li.innerHTML = `
-                    <div class="flex">
-                        <img class="user-img-like" src=${e.url} alt=${e.username}></img>
-                        <span class="us">${e.username}</span>
-                    </div>
-            `;
-            fragment.appendChild(li);
+        return new Promise((resolve, reject) => {
+            if (users == null ||
+                    users == undefined ||
+                    listContainer == null ||
+                    listContainer == undefined) {
+                reject("Error....");
+            } else {
+                let fragment = document.createDocumentFragment();
+                console.log(users);
+                users.forEach(e => {
+                    let li = document.createElement("li");
+                    li.setAttribute("class", "user-item");
+                    let adm = "";
+                    if (e.idTypeUser == 1) {
+                        adm = `
+                            <div class="card-user-type">
+                                <svg viewBox="0 0 16 16" class="svg-icon-admin" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-97.103 -44.137)">
+                                    <path d="M113.1,52.137a8,8,0,1,0-8,8,8,8,0,0,0,8-8" fill="#f0c419"></path>
+                                    <path d="M155.4,88.276l1.7,3.434,3.63.566-2.578,2.673.8,3.549L155.4,96.951,151.847,98.5l.8-3.549-2.578-2.673,3.63-.566Z" transform="translate(-50.299 -41.917)" fill="#ffe69f"></path></g>
+                                </svg>
+                                <span class="user-type_name">adm</span>
+                            </div>
+                        `;
+                    }
+                    li.innerHTML = `
+                        <div class="flex">
+                            <div class="card-user">
+                                ${adm}
+                                <img class="user-img-like" src=${e.url} alt=${e.username}></img>
+                            </div>
+                            <a href="perfil?id=${e.idPerson}"><span class="us">${e.username}</span></a>
+                        </div>
+                    `;
+                    fragment.appendChild(li);
+                });
+                listContainer.appendChild(fragment);
+                resolve();
+            }
         });
-
-        listContainer.appendChild(fragment);
     };
 
 };
